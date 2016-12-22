46f0279467b5373cc938617cedd3717b49c8913c
Trevor D. Miller <trevordmiller@icloud.com>
2016-12-21 21:30:05 -0700

    Add functionality with tests

---
+++ 6-functionality/src/index.test.js
@@ -0,0 +1,15 @@
+import sensitiveWords from '.'
+
+test('replaces blacklisted words with asterisks', () => (
+  expect(sensitiveWords(
+    'The name of the NX will be the Nintedo Switch',
+    ['switch']
+  )).toBe('The name of the NX will be the Nintedo ***')
+))
+
+test('replaces multiple instances of blacklisted words', () => (
+  expect(sensitiveWords(
+    'The name of the NX will be the Nintedo Switch. The switch will be awesome!',
+    ['switch']
+  )).toBe('The name of the NX will be the Nintedo ***. The *** will be awesome!')
+))
